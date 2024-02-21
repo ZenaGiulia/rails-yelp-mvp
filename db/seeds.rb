d7d7b78ac174dcb9c0220d3f9a1f747793e03482
Zena El Kurdi <zena.elkurdi@gmail.com>
2024-02-21 14:39:38 +0100

    rails_yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+genre = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do
+  Restaurant.create(
+     name: Faker::Book.title,
+     address: Faker::Book.author,
+     phone_number: "",
+     category: genre.sample
+   )
+ end
